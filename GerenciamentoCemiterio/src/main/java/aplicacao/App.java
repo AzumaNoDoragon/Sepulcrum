66f17ce60710894e2e3e16e777c0f2c047cc1774
Gabriel Afini <69328184+AzumaNoDoragon@users.noreply.github.com>
2022-10-15 15:56:12 -0300

    Update App.java

--- GerenciamentoCemiterio/src/main/java/aplicacao/App.java
+++ GerenciamentoCemiterio/src/main/java/aplicacao/App.java
@@ -3,7 +3,7 @@ package aplicacao;
 public class App {
 	public static void main(String args[]) {
 		CRUDGC crudGC = new CRUDGC();
-		//// Ordem de criação = id, pessoaPublica, cemiterio, nome, quadra, rua, numero,
+		// Ordem de criação = id, pessoaPublica, cemiterio, nome, quadra, rua, numero,
 		// certidaoObito, dataNascimento, dataObito;
 		crudGC.create();
 		crudGC.read();
