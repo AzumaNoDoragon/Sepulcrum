cfbab63e12a0fb994e09ca8f99888e5ccd4847e4
Gabriel Afini <69328184+AzumaNoDoragon@users.noreply.github.com>
2022-11-09 15:30:48 -0300

    Add files via upload

---
+++ CRUDGC-main/GerenciamentoCemiterio/src/main/java/dominio/Exumacao.java
@@ -0,0 +1,22 @@
+package dominio;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Exumacao implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idExumacao, idAdm, idTumulo, idDefunto;
+	private String motivoExumacao, informacaoExumacao;
+	private Calendar dataExumacao;
+
+	
+}
