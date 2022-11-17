b2579a49ed4b30e3dfde09d33aca2619f9b89f5d
Gabriel Afini <69328184+AzumaNoDoragon@users.noreply.github.com>
2022-11-17 20:32:33 -0300

    Delete CRUDGC-main directory

--- CRUDGC-main/GerenciamentoCemiterio/src/main/java/classePessoa/Defunto.java
+++
@@ -1,9 +0,0 @@
-package classePessoa;
-
-public class Defunto extends Pessoa {
-
-	private Integer idDefunto, certidaoObitoDefunto, dataNascimentoDefunto, dataObitoDefunto;
-	private Boolean pessoaPublicaDefunto;
-	private String causaMorteDefunto;
-
-}
