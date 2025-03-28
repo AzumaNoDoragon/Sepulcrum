af0d07e8a27de38afc30c785f27c3cfd916fc519
Gabriel Afini <gabrielsads@gmail.com>
2025-03-28 20:23:16 -0300

    Classe Gerenciador foi instanciado o controle de informações 'TelaGerenciador' e criado o switch de escolhas

--- Refact/src/gerenciador/Gerenciador.java
+++ Refact/src/gerenciador/Gerenciador.java
@@ -1,7 +1,41 @@
 package gerenciador;
 
 //import pessoas.*;
+import java.util.Scanner;
 
 public class Gerenciador {
+    private TelaGerenciador tg = new TelaGerenciador(new Scanner(System.in));
 
+    public void executar(){
+        int opc;
+
+        do{
+            opc = tg.menu();
+
+            switch (opc) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    break;
+                case 0:
+                    System.out.println("Saindo do Sistema...");
+                    break;
+                default:
+                    if(opc != 0){
+                        tg.msgOpcInvalida();
+                    }
+                    break;
+            }
+        }while (opc != 0);
+    }
 }
