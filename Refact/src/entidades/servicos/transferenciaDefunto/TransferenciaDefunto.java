9a73ae7ccd3a0d43d042574f62d97c044400fe25
Gabriel Afini <gabrielsads@gmail.com>
2025-04-03 19:40:42 -0300

    Adicionado idDefunto na entidade TransferenciaDefunto

--- Refact/src/entidades/servicos/transferenciaDefunto/TransferenciaDefunto.java
+++ Refact/src/entidades/servicos/transferenciaDefunto/TransferenciaDefunto.java
@@ -3,7 +3,15 @@ package entidades.servicos.transferenciaDefunto;
 import entidades.servicos.heranca.ServicoTumulo;
 
 public class TransferenciaDefunto extends ServicoTumulo {
-    private int idTumuloDestino;
+    private int idTumuloDestino, idDefunto;
+
+    public int getIdDefunto() {
+        return idDefunto;
+    }
+
+    public void setIdDefunto(int idDefunto) {
+        this.idDefunto = idDefunto;
+    }
 
     public int getIdTumuloDestino() {
         return idTumuloDestino;
