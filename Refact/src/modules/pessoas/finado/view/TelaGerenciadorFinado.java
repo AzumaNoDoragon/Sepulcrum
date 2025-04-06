14a53f55d81fb5cb7c9a7770cc43306b90d6a480
Gabriel Afini <gabrielsads@gmail.com>
2025-04-05 23:47:39 -0300

    set e get passado para o TelaGerenciadorFinado

--- Refact/src/modules/pessoas/finado/view/TelaGerenciadorFinado.java
+++ Refact/src/modules/pessoas/finado/view/TelaGerenciadorFinado.java
@@ -39,3 +39,37 @@ public class TelaGerenciadorFinado extends TelaMenu{
         jbVoltar = new JButton("Voltar");
     }
 }
+
+/*public void setDefunto(Finado d){
+    System.out.println("Digite o nome do Finado: ");
+    d.setNome(teclado.nextLine());
+    System.out.println("Digite o CPF: ");
+    d.setCpf(teclado.nextLine());
+    System.out.println("Digite o RG: ");
+    d.setRg(teclado.nextLine());
+    System.out.println("Digite a data de nascimento: ");
+    d.setDataNascimento(teclado.nextLine());
+    System.out.println("Digite a data de falecimento: ");
+    d.setDataFalecimento(teclado.nextLine());
+    System.out.println("Digite a causa da morte: ");
+    d.setCausaMorte(teclado.nextLine());
+    System.out.println("Digite o id do parente: ");
+    d.setIdParente(teclado.nextInt());teclado.nextLine();
+    System.out.println("Digite o id do tumulo: ");
+    d.setIdTumulo(teclado.nextInt());teclado.nextLine();
+}
+
+public void getDefunto(Finado d){
+    if(d != null){
+        System.out.println("nome do Finado: " + d.getNome());
+        System.out.println("CPF: " + d.getCpf());
+        System.out.println("RG: " + d.getRg());
+        System.out.println("Data de nascimento: " + d.getDataNascimento());
+        System.out.println("Data de falecimento: " + d.getDataFalecimento());
+        System.out.println("Causa da morte: " + d.getCausaMorte());
+        System.out.println("Id do parente: " + d.getIdParente());
+        System.out.println("Id do tumulo: " + d.getIdTumulo());
+    } else {
+        msgIdInexistente();
+    }
+}*/
