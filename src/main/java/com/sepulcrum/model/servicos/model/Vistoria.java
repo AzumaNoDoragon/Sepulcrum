b785becbf98706dc6a79712c7986eed7a71d4308
Gabriel Afini <gabrielsads@gmail.com>
2025-05-07 16:14:46 -0300

    Alterado a ordem dos parametros do construtor para que a posição dos botões na TelaServiço fosse em ordem

--- src/main/java/com/sepulcrum/model/servicos/model/Vistoria.java
+++ src/main/java/com/sepulcrum/model/servicos/model/Vistoria.java
@@ -1,10 +1,12 @@
 package com.sepulcrum.model.servicos.model;
 
+import java.sql.Date;
+
 import com.sepulcrum.model.servicos.model.utils.Servicos;
 
 public class Vistoria extends Servicos{
-    public Vistoria(String statusServicoStr, String tipoServicoStr, String descricao, String admCpf, String tumRua,
-            String tumNumero, String cemCnpj, int idServico) {
-        super(statusServicoStr, tipoServicoStr, descricao, admCpf, tumRua, tumNumero, cemCnpj, idServico);
+    public Vistoria(String tipoServicoStr, String descricao, String statusServicoStr, Date dataServico, String tumRua,
+            String tumNumero, String cemCnpj, String admCpf) {
+        super(tipoServicoStr, descricao, statusServicoStr, dataServico, tumRua, tumNumero, cemCnpj, admCpf);
     }
 }
