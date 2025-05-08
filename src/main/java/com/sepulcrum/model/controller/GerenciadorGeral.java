c6650ff0d7577c5bc4a1182afbcfaf735faf306f
Gabriel Afini <gabrielsads@gmail.com>
2025-05-08 19:55:10 -0300

    Implementado os métodos de set das classes, faltam complementar, pois não tive tempo hj e só adicionei o escopo

--- src/main/java/com/sepulcrum/model/controller/GerenciadorGeral.java
+++ src/main/java/com/sepulcrum/model/controller/GerenciadorGeral.java
@@ -1,5 +1,58 @@
 package com.sepulcrum.model.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sepulcrum.model.localidade.Cemiterio;
+import com.sepulcrum.model.localidade.Tumulo;
+import com.sepulcrum.model.localidade.utils.Localidade;
+import com.sepulcrum.model.pessoas.Adm;
+import com.sepulcrum.model.pessoas.Familiar;
+import com.sepulcrum.model.pessoas.Finado;
+import com.sepulcrum.model.pessoas.utils.Pessoas;
+import com.sepulcrum.model.pessoas.utils.PessoasVivas;
+import com.sepulcrum.model.view.TelaGerenciadorGeral;
+
 public class GerenciadorGeral {
-    
+    private List<Cemiterio> listC = new ArrayList<>();
+    private List<Tumulo> ListT = new ArrayList<>();
+    private List<Adm> ListA = new ArrayList<>();
+    private List<Familiar> ListFam = new ArrayList<>();
+    private List<Finado> ListFin = new ArrayList<>();
+
+    public void setLocalidade(TelaGerenciadorGeral tgg, Localidade l){
+
+    }
+
+    public void setPessoa(TelaGerenciadorGeral tgg, Pessoas p){
+
+    }
+
+    public void setPessoasVivas(TelaGerenciadorGeral tgg, PessoasVivas pv){
+
+    }
+
+    public void setCemiterio(TelaGerenciadorGeral tgg){
+
+    }
+
+    public void setTumulo(TelaGerenciadorGeral tgg){
+
+    }
+
+    public void setAdm(TelaGerenciadorGeral tgg){
+
+    }
+
+    public void setFamiliar(TelaGerenciadorGeral tgg){
+
+    }
+
+    public void setFinado(TelaGerenciadorGeral tgg){
+
+    }
+
+    public void getLocalidade(TelaGerenciadorGeral tgg, Localidade l){
+
+    }
 }
