656c6debf8647936bb9f0bb7ddc317d73c8d608e
Gabriel Afini <gabrielsads@gmail.com>
2025-05-09 20:50:56 -0300

    Implementando o que cada método faz

--- src/main/java/com/sepulcrum/model/controller/GerenciadorGeral.java
+++ src/main/java/com/sepulcrum/model/controller/GerenciadorGeral.java
@@ -1,5 +1,6 @@
 package com.sepulcrum.model.controller;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +12,7 @@ import com.sepulcrum.model.pessoas.Familiar;
 import com.sepulcrum.model.pessoas.Finado;
 import com.sepulcrum.model.pessoas.utils.Pessoas;
 import com.sepulcrum.model.pessoas.utils.PessoasVivas;
-import com.sepulcrum.model.view.TelaGerenciadorGeral;
+import com.sepulcrum.model.view.TelaRegistroGeral;
 
 public class GerenciadorGeral {
     private List<Cemiterio> listC = new ArrayList<>();
@@ -20,39 +21,124 @@ public class GerenciadorGeral {
     private List<Familiar> ListFam = new ArrayList<>();
     private List<Finado> ListFin = new ArrayList<>();
 
-    public void setLocalidade(TelaGerenciadorGeral tgg, Localidade l){
+    private void validarCampo(String campo, String nomeCampo){
+        if(campo == null || campo.trim().isEmpty()){
+            throw new IllegalArgumentException("Campo '" + nomeCampo + "' vazio ou inválido.");
+        }
+    }
+
+    private void validarCampo(Integer campo, String nomeCampo){
+        if(campo == null || campo == 0){
+            throw new IllegalArgumentException("Campo '" + nomeCampo + "' vazio ou inválido.");
+        }
+    }
+
+    private void validarCampo(Date campo, String nomeCampo){
+        if(campo == null){
+            throw new IllegalArgumentException("Campo '" + nomeCampo + "' vazio ou inválido.");
+        }
+    }
+
+    public void setLocalidade(TelaRegistroGeral trg, Localidade l){
+        validarCampo(trg.getJtfFourString(), "Rua");
+        validarCampo(trg.getJtfFiveString(), "Número");
+
+        l.setRua(trg.getJtfFourString());
+        l.setNumero(trg.getJtfFiveString());
+    }
+
+    public void setPessoa(TelaRegistroGeral trg, Pessoas p){
+        validarCampo(trg.getJtfOne(), "Nome");
+        validarCampo(trg.getJtfTwo(), "Cpf");
+        validarCampo(trg.getJtfFourDate(), "Data de Nascimento");
+
+        p.setNome(trg.getJtfOne());
+        p.setCpf(trg.getJtfTwo());
+        p.setRg(trg.getJtfThree());
+        p.setDataNascimento(trg.getJtfFourDate());
+    }
+
+    public void setPessoasVivas(TelaRegistroGeral trg, PessoasVivas pv){
+        validarCampo(trg.getJtfSixString(), "Email");
+        validarCampo(trg.getJtfSevenString(), "Telefone");
+
+        pv.setEmail(trg.getJtfSixString());
+        pv.setTelefone(trg.getJtfSevenString());
+    }
+
+    public void setCemiterio(TelaRegistroGeral trg){
+        Cemiterio c = new Cemiterio(
+            trg.getJtfFourString(), // rua
+            trg.getJtfFiveString(), // numero
+            trg.getJtfNine(),       // cnpj
+            trg.getJtfOne(),        // nome
+            trg.getJtfTwo(),        // estado
+            trg.getJtfThree(),      // cidade
+            trg.getJtfEight(),      // telefone
+            trg.getJtfSixString(),  // cep
+            trg.getJtfTen()         // admCpf
+        );
+
+        setLocalidade(trg, c);
+
 
     }
 
-    public void setPessoa(TelaGerenciadorGeral tgg, Pessoas p){
+    public void setTumulo(TelaRegistroGeral trg){
+        Tumulo t = new Tumulo(
+            null,
+            null,
+            null,
+            null,
+            null,
+            0
+        );
+
+        setLocalidade(trg, t);
 
     }
 
-    public void setPessoasVivas(TelaGerenciadorGeral tgg, PessoasVivas pv){
+    public void setAdm(TelaRegistroGeral trg){
+        Adm a = new Adm(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+
+        setPessoa(trg, a);
+        setPessoasVivas(trg, a);
 
     }
 
-    public void setCemiterio(TelaGerenciadorGeral tgg){
+    public void setFamiliar(TelaRegistroGeral trg){
+        Familiar fam = new Familiar(null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+
+        setPessoa(trg, fam);
+        setPessoasVivas(trg, fam);
 
     }
 
-    public void setTumulo(TelaGerenciadorGeral tgg){
+    public void setFinado(TelaRegistroGeral trg){
+        Finado fin = new Finado(null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
 
-    }
-
-    public void setAdm(TelaGerenciadorGeral tgg){
-
-    }
-
-    public void setFamiliar(TelaGerenciadorGeral tgg){
-
-    }
-
-    public void setFinado(TelaGerenciadorGeral tgg){
-
-    }
-
-    public void getLocalidade(TelaGerenciadorGeral tgg, Localidade l){
-
+        setPessoa(trg, fin);
     }
 }
