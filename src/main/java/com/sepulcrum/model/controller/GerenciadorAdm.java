50e597905cc64c556d1dc69955039caf07853f43
Gabriel Afini <gabrielsads@gmail.com>
2025-05-26 22:09:11 -0300

    Implementado os métodos de delete, update e verificação para a entidade adm

--- src/main/java/com/sepulcrum/model/controller/GerenciadorAdm.java
+++ src/main/java/com/sepulcrum/model/controller/GerenciadorAdm.java
@@ -3,8 +3,8 @@ package com.sepulcrum.model.controller;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-import com.sepulcrum.model.view.TelaGerenciadorGeral;
 import com.sepulcrum.model.view.TelaRegistroGeral;
+import com.sepulcrum.model.view.TelaSelectGeral;
 import com.sepulcrum.model.assets.ValidarCampos;
 import com.sepulcrum.model.pessoas.Adm;
 
@@ -23,8 +23,13 @@ public class GerenciadorAdm {
         vc.validarCampo(trg.getJtfNine(), "CNPJ");
     }
 
-    public void setAdm(TelaRegistroGeral trg){
+    public int setAdm(TelaRegistroGeral trg){
         validarCampo(trg);
+
+        int idCpf = verificaObjeto(Integer.parseInt(trg.getJtfTwo()));
+        if(idCpf == 1){
+            return 1;
+        }
 
         Adm a = new Adm(
             trg.getJtfOne(),         // nome
@@ -40,6 +45,16 @@ public class GerenciadorAdm {
         a.setRg(trg.getJtfThree()); // RG
 
         listA.add(a);
+        return 0;
+    }
+
+    public int verificaObjeto(int id){
+        Adm a = buscaAdm(id);
+        if(a != null){
+            JOptionPane.showMessageDialog(null, "Funcionario com este CPF já existe");
+            return 1;
+        }
+        return 0;
     }
 
     public Adm buscaAdm(int id) {
@@ -51,12 +66,12 @@ public class GerenciadorAdm {
         return null;
     }
 
-    public void selectAdm(int seletor, int seletorCrud, int id){
+    public void selectAdm(TelaSelectGeral tsg, int seletor, int seletorCrud, int id){
         Adm a = buscaAdm(id);
         if (a == null) {
             JOptionPane.showMessageDialog(null, "Coveiro com CPF " + id + " não encontrado.");
-            new TelaGerenciadorGeral(seletor);
         } else {
+            tsg.dispose();
             TelaRegistroGeral trg = new TelaRegistroGeral(seletor, seletorCrud, id);
             getAdm(trg, a);
         }    
@@ -72,5 +87,36 @@ public class GerenciadorAdm {
         trg.setJtfSeven(a.getTelefone());
         trg.setJtfEight(a.getCargo());
         trg.setJtfNine(a.getCemCnpj());
+    }
+
+    public int updateAdm(TelaRegistroGeral trg, int id){
+        validarCampo(trg);
+
+        Adm a = buscaAdm(id);
+
+        if(!trg.getJtfTwo().equals(a.getCpf())){
+            int cpfNovo = verificaObjeto(Integer.parseInt(trg.getJtfTwo()));
+            if(cpfNovo == 1){
+                return 1;
+            }
+        }
+
+        a.setNome(trg.getJtfOne());
+        a.setCpf(trg.getJtfTwo());
+        a.setRg(trg.getJtfThree());
+        a.setDataNascimento(trg.getJtfFourDate());
+        a.setDataContratacao(trg.getJtfFiveDate());
+        a.setEmail(trg.getJtfSix());
+        a.setTelefone(trg.getJtfSevenString());
+        a.setCargo(trg.getJtfEight());
+        a.setCemCnpj(trg.getJtfNine());
+
+        listA.add(a);
+        return 0;
+    }
+
+    public void deleteAdm(int id){
+        Adm a = buscaAdm(id);
+        listA.remove(a);
     }
 }
