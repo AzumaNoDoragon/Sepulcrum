3db7aa96cd8d1e5beb95aa4816aceb356603f5e4
Gabriel Afini <gabrielsads@gmail.com>
2025-06-10 18:40:09 -0300

    Corrigido a quantidade de parametros do create e as msg de erro

--- src/main/java/com/sepulcrum/DAO/DAOAdm.java
+++ src/main/java/com/sepulcrum/DAO/DAOAdm.java
@@ -4,9 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import com.sepulcrum.database.Conexao;
-import com.sepulcrum.model.localidade.Cemiterio;
 import com.sepulcrum.model.pessoas.Adm;
 
 public class DAOAdm {
@@ -17,7 +15,7 @@ public class DAOAdm {
             Connection connection = conn.getConnection();
             
             String sql = "INSERT INTO coveiro (COV_NOME, COV_CPF, COV_RG, COV_DATA_NASCIMENTO, COV_DATA_CONTRATACAO, COV_EMAIL, COV_TELEFONE, COV_CARGO, CEM_CNPJ)" +
-                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
             try(PreparedStatement stmt = connection.prepareStatement(sql)){
                 stmt.setString(1, a.getNome());
@@ -49,7 +47,7 @@ public class DAOAdm {
             Connection connection = conn.getConnection();
             
             String sql = "SELECT COV_CPF, COV_NOME, COV_RG, COV_DATA_NASCIMENTO, COV_EMAIL, COV_TELEFONE, COV_CARGO, COV_DATA_CONTRATACAO, CEM_CNPJ " +
-                        "FROM adm WHERE COV_CPF = ?";
+                        "FROM coveiro WHERE COV_CPF = ?";
 
             try(PreparedStatement stmt = connection.prepareStatement(sql)){
                 stmt.setString(1, Integer.toString(id));
@@ -59,13 +57,12 @@ public class DAOAdm {
                         Adm a = new Adm(
                             rs.getString("COV_NOME"),
                             rs.getString("COV_CPF"),
-                            rs.getString("COV_DATA_NASCIMENTO"),
-                            rs.getString("COV_DATA_CONTRATACAO"),
+                            rs.getDate("COV_DATA_NASCIMENTO"),
+                            rs.getDate("COV_DATA_CONTRATACAO"),
                             rs.getString("COV_EMAIL"),
                             rs.getString("COV_TELEFONE"),
                             rs.getString("COV_CARGO"),
-                            rs.getString("CEM_CNPJ"),
-                            
+                            rs.getString("CEM_CNPJ")
                         );
                         a.setRg(rs.getString("COV_RG"));
 
@@ -77,6 +74,63 @@ public class DAOAdm {
             }
         } catch (Exception e) {
             throw new IllegalArgumentException("Erro ao ler coveiro: " + e.getMessage(), e);
+        } finally {
+            conn.close();
+        }
+    }
+
+    public void updateAdm(Adm a, int id){
+        Conexao conn = new Conexao();
+        try {
+            conn.conectar();
+            Connection connection = conn.getConnection();
+            
+            String sql = "UPDATE coveiro SET COV_NOME = ?, COV_CPF = ?, COV_RG = ?, COV_DATA_NASCIMENTO = ?, COV_DATA_CONTRATACAO = ?, COV_EMAIL = ?, COV_TELEFONE = ?, COV_CARGO = ?, CEM_CNPJ = ? " + 
+                        "WHERE COV_CPF = ?;";
+
+            try(PreparedStatement stmt = connection.prepareStatement(sql)){
+                stmt.setString(1, a.getNome());
+                stmt.setString(2, a.getCpf());
+                stmt.setString(3, a.getRg());
+                stmt.setDate(4, a.getDataNascimento());
+                stmt.setDate(5, a.getDataContratacao());
+                stmt.setString(6, a.getEmail());
+                stmt.setString(7, a.getTelefone());
+                stmt.setString(8, a.getCargo());
+                stmt.setString(9, a.getCemCnpj());
+
+                stmt.setString(11, Integer.toString(id)); // Busca por este
+
+                int linhasAfetadas = stmt.executeUpdate();
+                if (linhasAfetadas == 0) {
+                    throw new SQLException("Falha ao alterar o Coveiro. Nenhuma linha afetada.");
+                }
+            }
+        } catch(Exception e){
+            throw new IllegalArgumentException("Não foi possivel inserir no banco: " + e.getMessage());
+        } finally {
+            conn.close();
+        }
+    }
+
+    public void deleteAdm(Adm a, int id){
+        Conexao conn = new Conexao();
+        try {
+            conn.conectar();
+            Connection connection = conn.getConnection();
+            
+            String sql = "DELETE FROM cemiterio WHERE COV_CPF = ?";
+
+            try(PreparedStatement stmt = connection.prepareStatement(sql)){
+                stmt.setString(1, Integer.toString(id));
+
+                int linhasAfetadas = stmt.executeUpdate();
+                if (linhasAfetadas == 0) {
+                    throw new SQLException("Falha ao deletar coveiro. Nenhuma linha afetada.");
+                }
+            }
+        } catch(Exception e){
+            throw new IllegalArgumentException("Não foi possivel inserir no banco: " + e.getMessage());
         } finally {
             conn.close();
         }
