10ed68bc0ef0dca1accbe6bf768873b078379165
Gabriel Afini <gabrielsads@gmail.com>
2025-06-10 20:36:01 -0300

    Removido o arrayList e adicionado o daoF para persistencia ao banco

--- src/main/java/com/sepulcrum/controller/GerenciadorFamiliar.java
+++ src/main/java/com/sepulcrum/controller/GerenciadorFamiliar.java
@@ -1,17 +1,16 @@
 package com.sepulcrum.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.JOptionPane;
-
+import com.sepulcrum.utils.ValidadorCamposInterface;
 import com.sepulcrum.utils.ValidarCampos;
 import com.sepulcrum.view.TelaRegistroGeral;
 import com.sepulcrum.view.TelaSelectGeral;
+import com.sepulcrum.dao.DAOFamiliar;
 import com.sepulcrum.model.pessoas.Familiar;
 
 public class GerenciadorFamiliar{
-    private static List<Familiar> listFam = new ArrayList<>();
-    private ValidarCampos vc = new ValidarCampos();
+    private DAOFamiliar daoF = new DAOFamiliar();
+    private ValidadorCamposInterface vc = new ValidarCampos();
 
     public void validarCampo(TelaRegistroGeral trg){
         vc.validarCampo(trg.getJtfOne(), "Nome");
@@ -26,11 +25,6 @@ public class GerenciadorFamiliar{
     public int setFamiliar(TelaRegistroGeral trg){
         validarCampo(trg);
 
-        int idCpf = verificarObjeto(Integer.parseInt(trg.getJtfTwo()));
-        if(idCpf == 1){
-            return 1;
-        }
-
         Familiar fam = new Familiar(
             trg.getJtfOne(),        // nome
             trg.getJtfTwo(),        // cpf
@@ -43,30 +37,12 @@ public class GerenciadorFamiliar{
         fam.setGrauParentesco(trg.getJtfFiveString()); // grauParentesco
         fam.setCertidaoObito(trg.getJtfEight());       // certidaoObito
 
-        listFam.add(fam);
+        daoF.createFamiliar(fam);
         return 0;
-    }
-
-    public int verificarObjeto(int id){
-        Familiar fam = buscaFamiliar(id);
-        if(fam != null){
-            JOptionPane.showMessageDialog(null, "Familiar com este CPF já existe!");
-            return 1;
-        }
-        return 0;
-    }
-
-    public Familiar buscaFamiliar(int id) {
-        for (Familiar fam : listFam) {
-            if (fam.getCpf().equals(Integer.toString(id))) {
-                return fam;
-            }
-        }
-        return null;
     }
 
     public void selectFamiliar(TelaSelectGeral tsg, int seletor, int seletorCrud, int id){
-        Familiar fam = buscaFamiliar(id);
+        Familiar fam = daoF.readFamiliar(id);
         if (fam == null) {
             JOptionPane.showMessageDialog(null, "Familiar com CPF " + id + " não encontrado.");
         } else {
@@ -90,14 +66,7 @@ public class GerenciadorFamiliar{
     public int updateFamiliar(TelaRegistroGeral trg, int id){
         validarCampo(trg);
 
-        Familiar fam = buscaFamiliar(id);
-
-        if(!trg.getJtfTwo().equals(fam.getCpf())){
-            int cpfNovo = verificarObjeto(Integer.parseInt(trg.getJtfTwo()));
-            if(cpfNovo == 1){
-                return 1;
-            }
-        }
+        Familiar fam = daoF.readFamiliar(id);
 
         fam.setNome(trg.getJtfOne());
         fam.setCpf(trg.getJtfTwo());
@@ -112,7 +81,7 @@ public class GerenciadorFamiliar{
     }
 
     public void deleteFamiliar(int id){
-        Familiar fam = buscaFamiliar(id);
-        listFam.remove(fam);
+        daoF.readFamiliar(id);
+        daoF.deleteFamiliar(id);;
     }
 }
