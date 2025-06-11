df5c50b61c3b746047b3a24620459e864637a5dc
Gabriel Afini <gabrielsads@gmail.com>
2025-06-11 14:16:09 -0300

    Reorganiza de MVC para  MVC por domínio e refina nomes para mais clareza

---
+++ src/main/java/com/sepulcrum/controller/localidade/CemiteiroController.java
@@ -0,0 +1,93 @@
+package com.sepulcrum.controller.localidade;
+
+import javax.swing.JOptionPane;
+import com.sepulcrum.utils.ValidadorCamposInterface;
+import com.sepulcrum.utils.ValidarCampos;
+import com.sepulcrum.view.TelaRegistroGeral;
+import com.sepulcrum.view.TelaSelectGeral;
+import com.sepulcrum.dao.localidade.CemiterioDAO;
+import com.sepulcrum.model.localidade.Cemiterio;
+
+public class CemiteiroController {
+    private CemiterioDAO daoC = new CemiterioDAO();
+    private ValidadorCamposInterface vc = new ValidarCampos();
+
+    public void validarCampo(TelaRegistroGeral trg){
+        vc.validarCampo(trg.getJtfOne(), "Nome");
+        vc.validarCampo(trg.getJtfTwo(), "Estado");
+        vc.validarCampo(trg.getJtfThree(), "Cidade");
+        vc.validarCampo(trg.getJtfFourString(), "Rua");
+        vc.validarCampo(trg.getJtfFiveString(), "Número");
+        vc.validarCampo(trg.getJtfSix(), "Cep");
+        vc.validarCampo(trg.getJtfEight(), "Telefone");
+        vc.validarCampo(trg.getJtfNine(), "CNPJ");
+        vc.validarCampo(trg.getJtfTen(), "Adiministrador");
+    }
+
+    public void setCemiterio(TelaRegistroGeral trg){
+        validarCampo(trg);
+
+        Cemiterio c = new Cemiterio(
+            trg.getJtfOne(),        // nome
+            trg.getJtfTwo(),        // estado
+            trg.getJtfThree(),      // cidade
+            trg.getJtfFourString(), // rua
+            trg.getJtfFiveString(), // numero
+            trg.getJtfSix(),        // cep
+            trg.getJtfEight(),      // telefone
+            trg.getJtfNine(),       // cnpj
+            trg.getJtfTen()         // admCpf
+        );
+        c.setCapacidadeMax(trg.getJtfSevenInt()); // Capacidade Max
+
+        daoC.createCemiterio(c);
+    }
+
+    public void selectCemiterio(TelaSelectGeral tsg, int seletor, int seletorCrud, int id){
+        Cemiterio c = daoC.readCemiterio(id);
+        if (c == null) {
+            JOptionPane.showMessageDialog(null, "Cemitério com CNPJ " + id + " não encontrado.");
+        } else {
+            tsg.dispose();
+            TelaRegistroGeral trg = new TelaRegistroGeral(seletor, seletorCrud, id);
+            getCemiterio(trg, c);
+        }
+    }
+
+    public void getCemiterio(TelaRegistroGeral trg, Cemiterio c){
+        trg.setJtfOne(c.getNome());
+        trg.setJtfTwo(c.getEstado());
+        trg.setJtfThree(c.getCidade());
+        trg.setJtfFour(c.getRua());
+        trg.setJtfFive(c.getNumero());
+        trg.setJtfSix(c.getCep());
+        trg.setJtfSeven(Integer.toString(c.getCapacidadeMax()));
+        trg.setJtfEight(c.getTelefone());
+        trg.setJtfNine(c.getCnpj());
+        trg.setJtfTen(c.getAdmCpf());
+    }
+
+    public void updateCemiterio(TelaRegistroGeral trg, int id){
+        validarCampo(trg);
+
+        Cemiterio c = daoC.readCemiterio(id);
+
+        c.setNome(trg.getJtfOne());
+        c.setEstado(trg.getJtfTwo());
+        c.setCidade(trg.getJtfThree());
+        c.setRua(trg.getJtfFourString());
+        c.setNumero(trg.getJtfFiveString());
+        c.setCep(trg.getJtfSix());
+        c.setCapacidadeMax(trg.getJtfSevenInt());
+        c.setTelefone(trg.getJtfEight());
+        c.setCnpj(trg.getJtfNine());
+        c.setAdmCpf(trg.getJtfTen());
+
+        daoC.updateCemiterio(c, id);
+    }
+
+    public void deleteCemiterio(int id){
+        daoC.readCemiterio(id);
+        daoC.deleteCemiterio(id);
+    }
+}
