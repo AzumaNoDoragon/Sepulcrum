80c572f1340ac9ca3d6fe88d5f3d3d97e56e3463
Gabriel Afini <gabrielsads@gmail.com>
2025-06-11 14:16:37 -0300

    Reorganiza de MVC para  MVC por domínio e refina nomes para mais clareza

---
+++ src/main/java/com/sepulcrum/controller/pessoa/FamiliarController.java
@@ -0,0 +1,84 @@
+package com.sepulcrum.controller.pessoa;
+
+import javax.swing.JOptionPane;
+import com.sepulcrum.utils.ValidadorCamposInterface;
+import com.sepulcrum.utils.ValidarCampos;
+import com.sepulcrum.view.TelaRegistroGeral;
+import com.sepulcrum.view.TelaSelectGeral;
+import com.sepulcrum.dao.pessoa.FamiliarDAO;
+import com.sepulcrum.model.pessoa.Familiar;
+
+public class FamiliarController{
+    private FamiliarDAO daoF = new FamiliarDAO();
+    private ValidadorCamposInterface vc = new ValidarCampos();
+
+    public void validarCampo(TelaRegistroGeral trg){
+        vc.validarCampo(trg.getJtfOne(), "Nome");
+        vc.validarCampo(trg.getJtfTwo(), "Cpf");
+        vc.validarCampo(trg.getJtfFourDate(), "Data de Nascimento");
+        vc.validarCampo(trg.getJtfFiveString(), "Grau de Parentesco");
+        vc.validarCampo(trg.getJtfSix(), "Email");
+        vc.validarCampo(trg.getJtfSevenString(), "Telefone");
+        vc.validarCampo(trg.getJtfEight(), "Certidão de Óbito");
+    }
+    
+    public void setFamiliar(TelaRegistroGeral trg){
+        validarCampo(trg);
+
+        Familiar fam = new Familiar(
+            trg.getJtfOne(),        // nome
+            trg.getJtfTwo(),        // cpf
+            trg.getJtfFourDate(),   // dataNascimento
+            trg.getJtfSix(),        // email
+            trg.getJtfSevenString() // telefone
+        );
+
+        fam.setRg(trg.getJtfThree());                  // RG
+        fam.setGrauParentesco(trg.getJtfFiveString()); // grauParentesco
+        fam.setCertidaoObito(trg.getJtfEight());       // certidaoObito
+
+        daoF.createFamiliar(fam);
+    }
+
+    public void selectFamiliar(TelaSelectGeral tsg, int seletor, int seletorCrud, int id){
+        Familiar fam = daoF.readFamiliar(id);
+        if (fam == null) {
+            JOptionPane.showMessageDialog(null, "Familiar com CPF " + id + " não encontrado.");
+        } else {
+            tsg.dispose();
+            TelaRegistroGeral trg = new TelaRegistroGeral(seletor, seletorCrud, id);
+            getFamiliar(trg, fam);
+        }
+    }
+
+    public void getFamiliar(TelaRegistroGeral trg, Familiar fam){
+        trg.setJtfOne(fam.getNome());
+        trg.setJtfTwo(fam.getCpf());
+        trg.setJtfThree(fam.getRg());
+        trg.setJtfFour(fam.getDataNascimento());
+        trg.setJtfSix(fam.getEmail());
+        trg.setJtfFive(fam.getGrauParentesco());
+        trg.setJtfSeven(fam.getTelefone());
+        trg.setJtfEight(fam.getCertidaoObito());
+    }
+
+    public void updateFamiliar(TelaRegistroGeral trg, int id){
+        validarCampo(trg);
+
+        Familiar fam = daoF.readFamiliar(id);
+
+        fam.setNome(trg.getJtfOne());
+        fam.setCpf(trg.getJtfTwo());
+        fam.setRg(trg.getJtfThree());
+        fam.setDataNascimento(trg.getJtfFourDate());
+        fam.setEmail(trg.getJtfSix());
+        fam.setGrauParentesco(trg.getJtfFiveString());
+        fam.setTelefone(trg.getJtfSevenString());
+        fam.setCertidaoObito(trg.getJtfEight());
+    }
+
+    public void deleteFamiliar(int id){
+        daoF.readFamiliar(id);
+        daoF.deleteFamiliar(id);;
+    }
+}
---
+++ src/main/java/com/sepulcrum/controller/pessoa/FinadoController.java
@@ -0,0 +1,84 @@
+package com.sepulcrum.controller.pessoa;
+
+import javax.swing.JOptionPane;
+import com.sepulcrum.utils.ValidadorCamposInterface;
+import com.sepulcrum.utils.ValidarCampos;
+import com.sepulcrum.view.TelaRegistroGeral;
+import com.sepulcrum.view.TelaSelectGeral;
+import com.sepulcrum.dao.pessoa.FinadoDAO;
+import com.sepulcrum.model.pessoa.Finado;
+
+public class FinadoController {
+    private FinadoDAO daoF = new FinadoDAO();
+    private ValidadorCamposInterface vc = new ValidarCampos();
+
+    public void validarCampo(TelaRegistroGeral trg){
+        vc.validarCampo(trg.getJtfFiveDate(), "Data Falecimento");
+        vc.validarCampo(trg.getJtfSix(), "Causa da Morte");
+        vc.validarCampo(trg.getJtfSevenString(), "Certidão de Óbito");
+        vc.validarCampo(trg.getJtfEight(), "Rua do Túmulo");
+        vc.validarCampo(trg.getJtfNine(), "Número do Túmulo");
+    }
+
+    public void setFinado(TelaRegistroGeral trg){
+        validarCampo(trg);
+
+        Finado fin = new Finado(
+            trg.getJtfOne(),         // nome
+            trg.getJtfTwo(),         // cpf
+            trg.getJtfFourDate(),    // dataNascimento
+            trg.getJtfFiveDate(),    // dataFalecimento
+            trg.getJtfSevenString(), // certidaoObito
+            trg.getJtfSix(),         // causaMorte
+            trg.getJtfEight(),       // tumRua
+            trg.getJtfNine()         // tumNumero
+        );
+        fin.setRg(trg.getJtfThree()); // Rg
+
+        daoF.createFinado(fin);
+    }
+
+    public void selectFinado(TelaSelectGeral tsg, int seletor, int seletorCrud, int id){
+        Finado fin = daoF.readFinado(id);
+        if (fin == null) {
+            JOptionPane.showMessageDialog(null, "Finado com Certidão de Óbito " + id + " não encontrado.");
+        } else {
+            tsg.dispose();
+            TelaRegistroGeral trg = new TelaRegistroGeral(seletor, seletorCrud, id);
+            getFinado(trg, fin);
+        }
+    }
+
+    public void getFinado(TelaRegistroGeral trg, Finado fin){
+        trg.setJtfOne(fin.getNome());
+        trg.setJtfTwo(fin.getCpf());
+        trg.setJtfThree(fin.getRg());
+        trg.setJtfFour(fin.getDataNascimento());
+        trg.setJtfFive(fin.getDataFalecimento());
+        trg.setJtfSix(fin.getCausaMorte());
+        trg.setJtfSeven(fin.getCertidaoObito());
+        trg.setJtfEight(fin.getTumRua());
+        trg.setJtfNine(fin.getTumNumero());
+    }
+
+    public void updateFinado(TelaRegistroGeral trg, int id){
+        validarCampo(trg);
+
+        Finado fin = daoF.readFinado(id);
+
+        fin.setNome(trg.getJtfOne());
+        fin.setCpf(trg.getJtfTwo());
+        fin.setRg(trg.getJtfThree());
+        fin.setDataNascimento(trg.getJtfFourDate());
+        fin.setDataFalecimento(trg.getJtfFiveDate());
+        fin.setCausaMorte(trg.getJtfSix());
+        fin.setCertidaoObito(trg.getJtfSevenString());
+        fin.setTumRua(trg.getJtfEight());
+        fin.setTumNumero(trg.getJtfNine());
+    }
+
+    public void deleteFinado(int id){
+        daoF.readFinado(id);
+        daoF.deleteFinado(id);
+    }
+}
