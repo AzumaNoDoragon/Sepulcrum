b51f362a875ca201c18b2b4663f85f19cc241e90
Gabriel Afini <gabrielsads@gmail.com>
2025-06-11 14:20:27 -0300

    Refina nomes para mais clareza

--- src/main/java/com/sepulcrum/view/TelaPrincipal.java
+++ src/main/java/com/sepulcrum/view/TelaPrincipal.java
@@ -1,7 +1,7 @@
 package com.sepulcrum.view;
 
 import javax.swing.JButton;
-import com.sepulcrum.model.servicos.view.TelaServico;
+import com.sepulcrum.model.servico.view.TelaServico;
 import com.sepulcrum.view.utils.TelaMenu;
 
 public class TelaPrincipal extends TelaMenu{
