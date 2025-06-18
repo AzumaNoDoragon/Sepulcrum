9d9a78ff82175ef6c0607260092b3ffdd265b560
Gabriel Afini <gabrielsads@gmail.com>
2025-06-18 04:08:10 -0300

    Removico cemCnpj

--- src/main/java/com/sepulcrum/model/pessoa/Adm.java
+++ src/main/java/com/sepulcrum/model/pessoa/Adm.java
@@ -5,18 +5,15 @@ import com.sepulcrum.model.pessoa.utils.PessoaViva;
 
 public class Adm extends PessoaViva{
     private Cargo cargo;
-    private String cemCnpj;
     private Date dataContratacao;
 
     private enum Cargo{
         Coveiro, Adm
     }
 
-    public Adm(String nome, String cpf, Date dataNascimento, Date dataContratacao, String email, String telefone, String cargoStr, 
-            String cemCnpj) {
+    public Adm(String nome, String cpf, Date dataNascimento, Date dataContratacao, String email, String telefone, String cargoStr) {
         super(nome, cpf, dataNascimento, email, telefone);
         setCargo(cargoStr);
-        this.cemCnpj = cemCnpj;
         this.dataContratacao = dataContratacao;
     }
 
@@ -30,14 +27,6 @@ public class Adm extends PessoaViva{
 
     public String getCargo() {
         return cargo.name();
-    }
-
-    public String getCemCnpj() {
-        return cemCnpj;
-    }
-
-    public void setCemCnpj(String cemCnpj) {
-        this.cemCnpj = cemCnpj;
     }
 
     public Date getDataContratacao() {
